f39a03865f3002d7a3091f60c089431185d6b0d0
Curry-ZHR <72328475+Curry-ZHR@users.noreply.github.com>
2020-10-18 01:31:11 +0800

    Update Thethird.c

--- Thethird.c
+++ Thethird.c
@@ -10,3 +10,22 @@ int main(void)
        puts("B不是A的约数");
        return 0;      
 }
+
+
+#include<stdio.h>
+int main(void)
+{
+    int A=54,B=12;
+    printf("请输入两个整数。");
+    printf("整数A：");
+    scanf("%d",&A);
+    printf("整数B：");
+    scanf("%d",&B);
+    if (A==B)
+       puts("A和B相等");
+    else if(A>B)
+       puts("A大于B");
+    else (A<B)
+       puts("A小于B");
+       return 0;      
+}
